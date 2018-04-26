846e5dadb84c57110b3a4db481892ad885044e20
anthonyflea <rusinski@student.agh.edu.pl>
2018-04-26 21:50:23 +0200

    Made deposit, withdraw and transfer methods available from bank class

--- src/main/java/pl/edu/agh/iet/katabank/Bank.java
+++ src/main/java/pl/edu/agh/iet/katabank/Bank.java
@@ -1,5 +1,6 @@
 package pl.edu.agh.iet.katabank;
 
+import java.math.BigDecimal;
 import java.util.Set;
 
 public class Bank {
@@ -12,5 +13,26 @@ public class Bank {
 
     public Set<Account> getAccountsForCustomer(Customer customer) {
         return accountsRepository.findAccountsForCustomer(customer);
+    }
+
+    public void deposit(Customer customer, Account account, BigDecimal depositAmount) {
+        checkOperationNotAllowed(customer, account, "Customer cannot deposit money to others account.");
+        account.deposit(depositAmount);
+    }
+
+    public void withdraw(Customer customer, Account account, BigDecimal withdrawAmount) {
+        checkOperationNotAllowed(customer, account, "Customer cannot withdraw money from others account.");
+        account.withdraw(withdrawAmount);
+    }
+
+    public void transfer(Customer customer, Account customersAccount, Account targetAccount, BigDecimal transferAmount) {
+        checkOperationNotAllowed(customer, customersAccount, "Customer cannot transfer money from others account.");
+        customersAccount.transfer(targetAccount, transferAmount);
+    }
+
+    private void checkOperationNotAllowed(Customer customer, Account account, String message) {
+        if (!getAccountsForCustomer(customer).contains(account)) {
+            throw new RuntimeException(message);
+        }
     }
 }
